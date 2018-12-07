aa42a916846261407b45ae072a30cd622565dc18
Mark Bastian <markbastian@gmail.com>
2018-12-06 19:39:58 -0700

    2018.06

--- src/adventofcode/year2018/day06.clj
+++ src/adventofcode/year2018/day06.clj
@@ -1,6 +1,7 @@
 (ns adventofcode.year2018.day06
   (:require [clojure.edn :as edn]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.data :refer [diff]]))
 
 (def input
   "1, 1
@@ -21,43 +22,68 @@
        (remove (fn [[_ freq]] (= 1 freq)))
        (map first)))
 
-(defn step [[[minx maxx] [miny maxy]] m]
-  (let [n (distinct (for [[k v] m [i j :as n] (neighbors k)
-                          :when (not (m n))
-                          ;:when (and (<= minx i maxx) (<= miny j maxy))
-                          ]
-                      [n v]))]
+(defn step [m]
+  (let [n (distinct (for [[k v] m n (neighbors k) :when (not (m n))] [n v]))]
     (conj
       (into {} n)
       (zipmap (overlaps n) (repeat nil))
       m)))
 
-(defn finites [[a b]]
-  (letfn [(f [grp] (into {} (map (fn [[g v]] [g (set (map first v))]) (group-by second grp))))]
-    (merge-with (fn [a b] (when (= a b) a)) (f a) (f b))))
+(defn simpify [m]
+  (let [m (dissoc (group-by second m) nil)]
+    (zipmap (keys m)
+            (map count (vals m)))))
+
+(defn input->pts [input]
+  (->> input (format "[%s]") edn/read-string (partition 2) (mapv vec)))
 
 (defn solution [input]
-  (let [p (->> input (format "[%s]") edn/read-string (partition 2) (mapv vec))
-        [xs ys] (apply mapv vector p)
-        minx (apply min xs)
-        maxx (apply max xs)
-        miny (apply min ys)
-        maxy (apply max ys)
-        g (vec (repeat (inc (- maxx minx)) (vec (repeat (inc (- maxy miny)) nil))))
-        res (->> (zipmap p (range))
-                 (iterate (partial step [[minx maxx] [miny maxy]]))
-                 (partition 2 1)
-                 (drop 50)
-                 first
-                 finites
-                 ;(drop-while (fn [[a b]] (not= a b)))
-                 ;ffirst
-                 ;(drop 20)
-                 ;first
-                 ;(group-by second)
-                 ;(map (fn [[k v]] [k (count (map first v))]))
-                 )]
-    res #_(apply mapv vector (reduce (fn [m [k v]] (assoc-in m (mapv - k [minx miny]) (or v '.))) g res))))
+  (let [p (input->pts input)]
+    (->> (zipmap p (range))
+         (iterate step)
+         (map simpify)
+         (partition 2 1)
+         (map (fn [[a b]] (last (diff a b))))
+         (filter identity)
+         (partition 20 1)
+         (some (fn [v] (when (apply = v) (first v)))))))
 
-;(solution input)
-;(->> "adventofcode/year2018/day06/input.txt" io/resource slurp solution)
+(defn dist [[a b] [c d]]
+  (+ (Math/abs ^int (- a c))
+     (Math/abs ^int (- b d))))
+
+(comment
+  (solution input)
+  ;2906
+  (->> "adventofcode/year2018/day06/input.txt" io/resource slurp solution))
+
+(defn roi-step [{:keys [frontier visited]}]
+  (let [new-frontier (distinct (remove visited (mapcat neighbors frontier)))]
+    {:frontier new-frontier
+     :visited  (into visited frontier)}))
+
+(defn total-dist [c pts]
+  (reduce + (for [p pts] (dist p c))))
+
+(defn filter-frontier [pts tol frontier]
+  (->> frontier
+       (map (fn [p] (total-dist p pts)))
+       (filter (fn [d] (< d tol)))))
+
+(defn region-size [tol input]
+  (let [pts (input->pts input)
+        s (reduce (fn [a b] (map + a b)) pts)
+        c (mapv #(quot % (count pts)) s)]
+    (->> {:frontier [c] :visited #{}}
+         (iterate roi-step)
+         (map :frontier)
+         (map (partial filter-frontier pts tol))
+         (take-while seq)
+         flatten
+         count)))
+
+(comment
+  ;16
+  (region-size 32 input)
+  ;50530
+  (->> "adventofcode/year2018/day06/input.txt" io/resource slurp (region-size 10000)))
