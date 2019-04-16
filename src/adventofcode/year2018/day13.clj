790decd86b5efed6566e37ab0bcade7d41260505
Mark Bastian <mbastian@clearwateranalytics.com>
2019-04-16 12:12:14 -0600

    comments

--- src/adventofcode/year2018/day13.clj
+++ src/adventofcode/year2018/day13.clj
@@ -2,11 +2,6 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as cs]))
 
-(def moves {:left :straight :straight :right :right :left})
-
-;up and down are screen coords
-(def dirs {\> [0 1] \< [0 -1] \^ [-1 0] \v [1 0] \X [0 0]})
-
 (defn split-cars [s]
   (reduce
     (fn [m [c spc]]
@@ -28,6 +23,13 @@
             c (range (count line)) :let [v (line c)]
             :when ((complement #{\space}) v)]
         [[r c] v]))))
+
+(def input1 (parse-input "adventofcode/year2018/day13/test_input1.txt"))
+(def input2 (parse-input "adventofcode/year2018/day13/test_input2.txt"))
+(def input3 (parse-input "adventofcode/year2018/day13/test_input3.txt"))
+(def input (parse-input "adventofcode/year2018/day13/input.txt"))
+
+(def moves {:left :straight :straight :right :right :left})
 
 (defn new-dir [{:keys [dir turn] :as car} track]
   (if (= \+ track)
@@ -58,19 +60,27 @@
 (defn remove-crashes [{:keys [queue] :as m}]
   (cond-> m (empty? queue) (dissoc :crashes)))
 
+;up and down are screen coords
+(def dirs {\> [0 1] \< [0 -1] \^ [-1 0] \v [1 0] \X [0 0]})
+
 (defn car-step [{:keys [cars track crashes] [f & r] :queue :as m}]
+  ;Handle cars in the queue
   (if f
     (let [{:keys [dir] :as car} (cars f)
           new-pos (mapv + f (dirs dir))
           m (-> m (update :cars dissoc f) (assoc :queue r))]
       (cond
+        ;A crash exists at this location, just add the car to the existing crashes
         (get crashes new-pos) (update-in m [:crashes new-pos] (conj car))
+        ;There is a car in the new location - instantiate a crash
         (cars new-pos) (-> m
                            (update-in [:crashes new-pos] conj car)
                            (update :cars dissoc new-pos)
                            (update-in [:crashes new-pos] conj (cars new-pos))
                            (update :queue (partial remove #{new-pos})))
+        ;The car safely moves forward
         :else (assoc-in m [:cars new-pos] (new-dir car (track new-pos)))))
+    ;If there are no cars in the queue, re-enqueue all the cars
     (assoc m :queue (sort-by (juxt second first) (keys cars)))))
 
 (defn find-first-crash-site [input]
@@ -88,11 +98,6 @@
        (map :cars)
        (remove (fn [cars] (> (count cars) 1)))
        first))
-
-(def input1 (parse-input "adventofcode/year2018/day13/test_input1.txt"))
-(def input2 (parse-input "adventofcode/year2018/day13/test_input2.txt"))
-(def input3 (parse-input "adventofcode/year2018/day13/test_input3.txt"))
-(def input (parse-input "adventofcode/year2018/day13/input.txt"))
 
 (comment
   ;[3 0]
