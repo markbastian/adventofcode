03d51419dee291410e2b55d3d74e2ab24705ac3d
Mark Bastian <mbastian@clearwateranalytics.com>
2020-07-07 22:33:33 -0600

    Updated solution

--- src/adventofcode/year2017/day05.clj
+++ src/adventofcode/year2017/day05.clj
@@ -11,30 +11,26 @@
        (mapv (comp edn/read-string cs/trim))))
 
 (defn step [[offsets v]]
-  (let [n (+ v (offsets v))]
-    [(update offsets v inc) n]))
+  [(update offsets v inc) (+ v (offsets v))])
 
-(defn jump-steps [jumps]
-  (count
-    (take-while
-      (fn [[s v]] (contains? s v))
-      (iterate step [jumps 0]))))
-
-;354121
-;(jump-steps jumps)
-
-(defn step2 [[offsets v]]
-  (let [n (+ v (offsets v))]
-    [(update offsets v #((if (>= % 3) dec inc) %)) n]))
-
-(defn jump-steps2 [jumps]
-  (count
-    (take-while
-      (fn [[s v]] (contains? s v))
-      (iterate step2 [jumps 0]))))
+(defn jump-steps [step-fn jumps]
+  (->> (iterate step-fn [jumps 0])
+       (take-while (fn [[s v]] (contains? s v)))
+       count))
 
 (comment
-  (jump-steps2 [0 3 0 1 -3])
+  (step (step (step [jumps 0])))
+  (take 20 (map second (iterate step [jumps 0])))
+  ;354121
+  (jump-steps step jumps))
+
+(defn step2 [[offsets v]]
+  [(update offsets v (fn [i] ((if (>= i 3) dec inc) i)))
+   (+ v (offsets v))])
+
+(comment
+  ;10
+  (jump-steps step2 [0 3 0 1 -3])
   ;27283023 - takes a while
-  (jump-steps2 jumps))
+  (jump-steps step2 jumps))
 
