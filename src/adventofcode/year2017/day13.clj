e8dc4c32e5b9cca91ff3b0fae7c75bf34faf32d3
Mark Bastian <markbastian@gmail.com>
2020-07-12 07:54:23 -0600

    Cleaned up 2017.13.

--- src/adventofcode/year2017/day13.clj
+++ src/adventofcode/year2017/day13.clj
@@ -20,11 +20,8 @@
     (update scanner :scanner-pos op)))
 
 (defn parse-input [s]
-  (->> s
-       cs/split-lines
-       (mapv (fn [line]
-               (->> (cs/split line #":")
-                    (mapv (comp edn/read-string cs/trim)))))))
+  (letfn [(f [line] (mapv (comp edn/read-string cs/trim) (cs/split line #":")))]
+    (mapv f (cs/split-lines s))))
 
 (defn build-firewall [desc]
   (let [[scanner-range] (last desc)
@@ -32,39 +29,40 @@
     (mapv (fn [i] (create-scanner (m i 0))) (range (inc scanner-range)))))
 
 (defn firewall-seq [firewall]
-  (->> firewall
-       (iterate #(mapv scanner-step %))
-       (map (fn [fw] (mapv (fn [scanner] (dissoc scanner :op)) fw)))))
+  (iterate #(mapv scanner-step %) firewall))
 
-(defn severity [[f :as firewall-seq]]
-  (->> (map vector (range (count f)) firewall-seq)
-       (map (fn [[packet-pos fw]]
-              (let [{:keys [scanner-pos scanner-range]} (fw packet-pos)]
-                (if (= 0 scanner-pos)
-                  (* packet-pos scanner-range)
-                  0))))
-       (reduce +)))
+(defn packet-run-seq [[firewall :as firewall-seq]]
+  (->> firewall-seq
+       (map #(mapv :scanner-pos %))
+       (partition (count firewall) 1)
+       (map (fn [s] (map #(%1 %2) s (range))))))
 
-(defn severity-seq [input]
-  (let [firewall (build-firewall input)]
-    (map severity (iterate rest (firewall-seq firewall)))))
+(defn severity [firewall-depths packet-run]
+  (let [d (map-indexed (fn [i v] (if (zero? (or v ##NaN)) i 0)) packet-run)]
+    (reduce + (map * firewall-depths d))))
+
+(defn severity-seq [firewall]
+  (let [firewall-depths (map (comp #(or % 0) :scanner-range) firewall)]
+    (->> firewall
+         firewall-seq
+         packet-run-seq
+         (map (partial severity firewall-depths)))))
+
+(defn caught? [s]
+  (true? (some zero? (filter identity s))))
+
+(defn safe-steps [firewall]
+  (reduce
+    (fn [i packet-run] (if (caught? packet-run) (inc i) (reduced i)))
+    0
+    (->> firewall firewall-seq packet-run-seq)))
 
 (def example-data "0: 3\n1: 2\n4: 4\n6: 4")
 (def data (-> "adventofcode/year2017/day13/input.txt" io/resource slurp))
 
-(defn safe-steps [firewall]
-  (->> firewall
-       firewall-seq
-       (map #(mapv :scanner-pos %))
-       (partition (count firewall) 1)
-       (map (fn [s] (map #(%1 %2) s (range))))
-       (map (fn [s] (some zero? (filter identity s))))
-       (take-while true?)
-       count))
-
 (comment
-  (= 24 (-> example-data parse-input severity-seq first))
-  (= 1704 (-> data parse-input severity-seq first))
+  (= 24 (-> example-data parse-input build-firewall severity-seq first))
+  (= 1704 (-> data parse-input build-firewall severity-seq first))
 
   (= 10 (->> example-data parse-input build-firewall safe-steps))
   ;Note - this can take a very long time.
