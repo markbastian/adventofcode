0938391609f349a0b4806d632ad7a2dd0d9000f6
Mark Bastian <mbastian@clearwateranalytics.com>
2020-08-11 16:09:34 -0600

    About to attempt native compilation.

--- project.clj
+++ project.clj
@@ -4,8 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.async "0.6.532"]
-                 [datascript "0.18.7"]
-                 [org.clojure/core.logic "0.8.11"]
-                 [quil "3.1.0"]
+                 [org.clojure/core.async "1.3.610"]
+                 [datascript "1.0.0"]
                  [org.clojure/data.priority-map "1.0.0"]])
