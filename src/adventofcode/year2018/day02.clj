ddc92821ea8e158c0f1f69658d962aac0d09efc0
Mark Bastian <mbastian@clearwateranalytics.com>
2020-09-23 12:57:51 -0600

    formatting

--- src/adventofcode/year2018/day02.clj
+++ src/adventofcode/year2018/day02.clj
@@ -2,10 +2,6 @@
   (:require [clojure.string :as cs]
             [clojure.java.io :as io]))
 
-(def test-input-1 "abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab")
-(def test-input-2 "abcde\nfghij\nklmno\npqrst\nfguij\naxcye\nwvxyz")
-(def input (-> "adventofcode/year2018/day02/input.txt" io/resource slurp))
-
 (defn checksum [input]
   (->> input
        cs/split-lines
@@ -14,20 +10,25 @@
        vals
        (apply *)))
 
-(comment
-  (time (= 12 (checksum test-input-1)))
-  (time (= 9633 (checksum input))))
-
 (defn similars [a b]
   (let [x (cs/join (map (fn [a b] (when (= a b) a)) a b))]
     (when (= (dec (count a)) (count x)) x)))
 
 (defn one-off [input]
   (loop [[f & r] (cs/split-lines input)]
-    (if-some [s (some #(similars f %) r)]
-      s
-      (recur r))))
+    (when f
+      (if-some [s (some #(similars f %) r)]
+        s
+        (recur r)))))
+
+(def test-input-1 "abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab")
+(def test-input-2 "abcde\nfghij\nklmno\npqrst\nfguij\naxcye\nwvxyz")
+(def input (-> "adventofcode/year2018/day02/input.txt" io/resource slurp))
 
 (comment
+  ;Part 1
+  (time (= 12 (checksum test-input-1)))
+  (time (= 9633 (checksum input)))
+  ;Part 2
   (time (= "fgij" (one-off test-input-2)))
   (time (= "lujnogabetpmsydyfcovzixaw" (one-off input))))
