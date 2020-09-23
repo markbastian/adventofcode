3ce55f342816a3495700e456ea52ac1dfb152bb6
Mark Bastian <mbastian@clearwateranalytics.com>
2020-09-23 07:21:17 -0600

    cleanup

--- src/adventofcode/year2018/day02.clj
+++ src/adventofcode/year2018/day02.clj
@@ -2,43 +2,32 @@
   (:require [clojure.string :as cs]
             [clojure.java.io :as io]))
 
-(def input
-  "abcdef
-bababc
-abbcde
-abcccd
-aabcdd
-abcdee
-ababab")
+(def test-input-1 "abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab")
+(def test-input-2 "abcde\nfghij\nklmno\npqrst\nfguij\naxcye\nwvxyz")
+(def input (-> "adventofcode/year2018/day02/input.txt" io/resource slurp))
 
 (defn checksum [input]
   (->> input
        cs/split-lines
-       (map cs/trim)
        (mapcat (comp #(remove #{1} %) distinct vals frequencies))
        frequencies
        vals
        (apply *)))
 
 (comment
-  (->> "adventofcode/year2018/day02/input.txt" io/resource slurp checksum))
-
-(def input2 "abcde\nfghij\nklmno\npqrst\nfguij\naxcye\nwvxyz")
+  (time (= 12 (checksum test-input-1)))
+  (time (= 9633 (checksum input))))
 
 (defn similars [a b]
   (let [x (cs/join (map (fn [a b] (when (= a b) a)) a b))]
     (when (= (dec (count a)) (count x)) x)))
 
 (defn one-off [input]
-  (loop [[f & r] (->> input cs/split-lines)]
+  (loop [[f & r] (cs/split-lines input)]
     (if-some [s (some #(similars f %) r)]
       s
       (recur r))))
 
-
 (comment
-  ;"lujnogabetpmsydyfcovzixaw"
-  (->> "adventofcode/year2018/day02/input.txt"
-       io/resource
-       slurp
-       one-off))
+  (time (= "fgij" (one-off test-input-2)))
+  (time (= "lujnogabetpmsydyfcovzixaw" (one-off input))))
--- src/adventofcode/year2018/day03.clj
+++ src/adventofcode/year2018/day03.clj
@@ -4,6 +4,7 @@
             [clojure.set :as s]))
 
 (def input "#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2")
+(def test-input (->> "adventofcode/year2018/day03/input.txt" io/resource slurp))
 
 (defn parse-claims [input]
   (->> (re-seq #"#(\d+)\s+@\s+(\d+),(\d+):\s+(\d+)x(\d+)" input)
@@ -11,39 +12,28 @@
               (zipmap [:claim :x :y :w :h]
                       (map edn/read-string [c x y w h]))))))
 
-(defn overlapping-square-count [input]
+(defn overlapping-squares [input]
   (->>
-    (for [{:keys [claim x y w h]} (->> input parse-claims)
+    (for [{:keys [x y w h]} (parse-claims input)
           i (range x (+ x w))
           j (range y (+ y h))] [i j])
     frequencies
-    (remove (comp zero? dec second))
-    count))
+    (remove (comp zero? dec second))))
+
+(defn overlapping-square-count [input]
+  (count (overlapping-squares input)))
 
 (comment
   (overlapping-square-count input)
-  (->> "adventofcode/year2018/day03/input.txt"
-       io/resource
-       slurp
-       overlapping-square-count))
+  (time (= 105231 (overlapping-square-count test-input))))
 
 (defn valid-claims [input]
-  (let [f (->>
-            (for [{:keys [x y w h]} (->> input parse-claims)
-                  i (range x (+ x w))
-                  j (range y (+ y h))] [i j])
-            frequencies
-            (remove (comp zero? dec second))
-            (map first)
-            set)]
-    (for [{:keys [claim x y w h]} (->> input parse-claims)
-          :let [coords (set (for [i (range x (+ x w)) j (range y (+ y h))] [i j]))]
-          :when (empty? (s/intersection f coords))
-          ]
-      {:claim claim})))
+  (let [f (->> (overlapping-squares input) (map first) set)]
+    (first
+      (for [{:keys [claim x y w h]} (->> input parse-claims)
+            :let [coords (set (for [i (range x (+ x w)) j (range y (+ y h))] [i j]))]
+            :when (empty? (s/intersection f coords))]
+        {:claim claim}))))
 
 (comment
-  (->> "adventofcode/year2018/day03/input.txt"
-       io/resource
-       slurp
-       valid-claims))
+  (time (= {:claim 164} (valid-claims test-input))))
