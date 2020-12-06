b142da3a2a865da8f9ec86cd92d41b70d5969358
Mark Bastian <markbastian@gmail.com>
2020-12-05 20:49:12 -0700

    2020.day02 amended

--- src/adventofcode/year2020/day02.clj
+++ src/adventofcode/year2020/day02.clj
@@ -2,31 +2,26 @@
   (:require [clojure.string :as cs]
             [clojure.java.io :as io]))
 
-(def test-input (->> (io/resource "adventofcode/year2020/day02/test-input.txt")
-                     slurp
-                     cs/split-lines))
+(defn input->lines [resource]
+  (->> resource io/resource slurp cs/split-lines))
 
-(def input (->> (io/resource "adventofcode/year2020/day02/input.txt")
-                slurp
-                cs/split-lines))
+(def test-input (input->lines "adventofcode/year2020/day02/test-input.txt"))
+
+(def input (input->lines "adventofcode/year2020/day02/input.txt"))
 
 (def rgx #"(\d+)-(\d+)\s+(\p{Alpha})\s*:\s*(\p{Alpha}+)")
 
 (defn parse-line [pattern]
-  (let [[_ lo hi [c] s] (re-matches rgx pattern)
-        lo (Long/parseLong lo)
-        hi (Long/parseLong hi)]
-    [lo hi c (vec s)]))
+  (let [[_ lo hi [c] s] (re-matches rgx pattern)]
+    [(Long/parseLong lo) (Long/parseLong hi) c (vec s)]))
 
 (defn valid-password-part1? [pattern]
-  (let [[lo hi c s] (parse-line pattern)
-        freqs (frequencies s)
-        freq (freqs c 0)]
-    (<= lo freq hi)))
+  (let [[lo hi c s] (parse-line pattern)]
+    (<= lo ((frequencies s) c 0) hi)))
 
 (defn valid-password-part2? [pattern]
   (let [[lo hi c s] (parse-line pattern)]
-    (->> [lo hi] (map dec) (map s) (filter #{c}) count (= 1))))
+    (->> [lo hi] (map (comp s dec)) (filter #{c}) count (= 1))))
 
 (comment
   ;Part 1
@@ -35,5 +30,4 @@
 
   ;Part 2
   (time (= 1 (count (filter valid-password-part2? test-input))))
-  (time (= 441 (count (filter valid-password-part2? input))))
-  )
+  (time (= 441 (count (filter valid-password-part2? input)))))
