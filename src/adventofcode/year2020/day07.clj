43e2f3a962665afbc29759cff7e317008931cfd2
Mark Bastian <markbastian@gmail.com>
2020-12-15 17:07:34 -0700

    day07 getting caught up

--- src/adventofcode/year2020/day07.clj
+++ src/adventofcode/year2020/day07.clj
@@ -2,25 +2,31 @@
   (:require [clojure.string :as cs]
             [clojure.java.io :as io]))
 
+(defn parse-contents [s]
+  (when-not (cs/includes? s "no other")
+    (->> (cs/split (cs/replace s #"\s*\." "") #"\s*,\s*")
+         (map (fn [s]
+                (when-not (= "no other" s)
+                  (let [[n m c] (cs/split s #"\s+")]
+                    {:color c :modifier m :count (Long/parseLong n)})))))))
+
 (defn parse-line [s]
-  (let [[f & r] (cs/split (cs/replace s #"bags?" "") #"\s*contain\s*")
+  (let [[f r] (cs/split (cs/replace s #"bags?" "") #"\s*contain\s*")
         [m c] (cs/split f #"\s+")]
-    [{:color c :modifier m} r]))
+    [{:color c :modifier m} (parse-contents r)]))
 
 (defn read-input [s]
   (->> (io/resource s)
        slurp
        cs/split-lines
-       (map parse-line)))
-
-(let [s "light red bags contain 1 bright white bag, 2 muted yellow bags."
-      [f r] (cs/split (cs/replace s #"bags?" "") #"\s*contain\s*")
-      [m c] (cs/split f #"\s+")
-      r (->> (cs/split (cs/replace r #"\s*\." "") #"\s*,\s*")
-             (map (fn [s]
-                    (let [[n m c] (cs/split s #"\s+")]
-                      {:color c :modifier m :count (Long/parseLong n)}))))]
-  [{:color c :modifier m} r])
+       (map parse-line)
+       (into {})))
 
 (def test-input (read-input "adventofcode/year2020/day07/test-input.txt"))
 (def input (read-input "adventofcode/year2020/day07/input.txt"))
+
+(reduce
+  (fn [m [k vs]]
+    (reduce (fn [m v] (assoc m v k)) m vs))
+  {}
+  test-input)
