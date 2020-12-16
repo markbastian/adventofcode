85c7e3c44dfaab5314d0108c1009e8a6c586657f
Mark Bastian <markbastian@gmail.com>
2020-12-15 17:59:42 -0700

    2020.day07**

--- src/adventofcode/year2020/day07.clj
+++ src/adventofcode/year2020/day07.clj
@@ -1,6 +1,7 @@
 (ns adventofcode.year2020.day07
   (:require [clojure.string :as cs]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io])
+  (:import (clojure.lang PersistentQueue)))
 
 (defn parse-contents [s]
   (when-not (cs/includes? s "no other")
@@ -25,8 +26,35 @@
 (def test-input (read-input "adventofcode/year2020/day07/test-input.txt"))
 (def input (read-input "adventofcode/year2020/day07/input.txt"))
 
-(reduce
-  (fn [m [k vs]]
-    (reduce (fn [m v] (assoc m v k)) m vs))
-  {}
-  test-input)
+(defn neighbors [input]
+  (reduce
+    (fn [m [o i]] (update m i (comp set conj) o))
+    {}
+    (for [[outer vs] input v vs] [outer (dissoc v :count)])))
+
+(defn visit [input]
+  (let [neighbors-fn (neighbors input)]
+    (loop [frontier (conj PersistentQueue/EMPTY {:color "gold", :modifier "shiny"})
+           visited #{}]
+      (if-some [n (peek frontier)]
+        (let [r (pop frontier)
+              new-neighbors (remove visited (neighbors-fn n))]
+          (recur (into r new-neighbors) (conj visited n)))
+        visited))))
+
+(comment
+  (time (= 4 (dec (count (visit test-input)))))
+  (time (= 151 (dec (count (visit input))))))
+
+(defn dfs [bag input]
+  (if-some [bags (input bag)]
+    (->> bags
+         (map (fn [{:keys [count] :as bag}]
+                (* count (dfs (dissoc bag :count) input))))
+         (reduce +)
+         inc)
+    1))
+
+(comment
+  (time (= 32 (dec (dfs {:color "gold", :modifier "shiny"} test-input))))
+  (time (= 41559 (dec (dfs {:color "gold", :modifier "shiny"} input)))))
